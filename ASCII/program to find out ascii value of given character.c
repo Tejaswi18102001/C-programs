f9b07797da3d7b22e2f43361cc75b7ba0b3a7452
Tejaswi18102001 <91786228+Tejaswi18102001@users.noreply.github.com>
2021-11-12 23:59:55 +0530

    Add files via upload

---
+++ ASCII/program to find out ascii value of given character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='\0';
+
+    printf("\n Enter a character = ");
+    ch=getche ();
+
+    printf("\n \n ASCII VALUE OF %c is = %d ",ch,ch);
+
+    getch();
+    return 0;
+}
---
+++ ASCII/program to find out charcter for a given ascii value.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int val=0;
+
+    printf("Enter a value=");
+    scanf("%d",&val);
+
+    printf("\n \n charcter for ACSII VALUE %d is =%c .",val,val);
+    getch();
+    return 0;
+}
