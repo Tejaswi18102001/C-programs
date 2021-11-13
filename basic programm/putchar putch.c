8a2845e51b57e3ce871002d55470c6c9c1482677
Tejaswi18102001 <91786228+Tejaswi18102001@users.noreply.github.com>
2021-11-13 14:48:46 +0530

    Add files via upload

---
+++ basic programm/putchar putch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch ='Q',Grade='C',Group='X';
+
+    putch('\n');
+    putchar('\t');
+
+    printf("Welcome to putchar/putch demonstration => \n\n ");
+
+    putch(ch);
+    putch('\n');
+
+    putchar(ch);
+    putch('\n');
+
+    putch(Group);
+    putch('\n');
+
+    putch('S');
+    putch('\n');
+
+    putch('KARAD');
+    putch('\n');
+
+    putch('Grade');
+    putch('\n');
+
+    getch();
+    return 0;
+
+
+}
